ea099f24bf704439f767367112319d0aa50ad51d
Pranav Mangal <pranav.mangal.7@gmail.com>
2024-09-19 04:06:32 +0530

    Use DOM methods for building SVG and update manifest

--- content.js
+++ content.js
@@ -1,7 +1,22 @@
-function fileZipSVG(forSidebar) {
-  const marginClass = forSidebar ? "mr-2" : "mr-1";
+function fizeZipSVG(forSidebar) {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute(
+    "class",
+    `octicon octicon-file-zip ${forSidebar ? "mr-2" : "mr-1"}`
+  );
+  svg.setAttribute("viewBox", "0 0 16 16");
+  svg.setAttribute("width", "16");
+  svg.setAttribute("height", "16");
 
-  return `<svg xmlns="http://www.w3.org/2000/svg" class="octicon octicon-file-zip ${marginClass}" viewBox="0 0 16 16" width="16" height="16"><path d="M3.5 1.75v11.5c0 .09.048.173.126.217a.75.75 0 0 1-.752 1.298A1.748 1.748 0 0 1 2 13.25V1.75C2 .784 2.784 0 3.75 0h5.586c.464 0 .909.185 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v8.586A1.75 1.75 0 0 1 12.25 15h-.5a.75.75 0 0 1 0-1.5h.5a.25.25 0 0 0 .25-.25V4.664a.25.25 0 0 0-.073-.177L9.513 1.573a.25.25 0 0 0-.177-.073H7.25a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5h-3a.25.25 0 0 0-.25.25Zm3.75 8.75h.5c.966 0 1.75.784 1.75 1.75v3a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1-.75-.75v-3c0-.966.784-1.75 1.75-1.75ZM6 5.25a.75.75 0 0 1 .75-.75h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 6 5.25Zm.75 2.25h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 6.75A.75.75 0 0 1 8.75 6h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 6.75ZM8.75 3h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 9.75A.75.75 0 0 1 8.75 9h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 9.75Zm-1 2.5v2.25h1v-2.25a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25Z"></path></svg>`;
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute(
+    "d",
+    "M3.5 1.75v11.5c0 .09.048.173.126.217a.75.75 0 0 1-.752 1.298A1.748 1.748 0 0 1 2 13.25V1.75C2 .784 2.784 0 3.75 0h5.586c.464 0 .909.185 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v8.586A1.75 1.75 0 0 1 12.25 15h-.5a.75.75 0 0 1 0-1.5h.5a.25.25 0 0 0 .25-.25V4.664a.25.25 0 0 0-.073-.177L9.513 1.573a.25.25 0 0 0-.177-.073H7.25a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5h-3a.25.25 0 0 0-.25.25Zm3.75 8.75h.5c.966 0 1.75.784 1.75 1.75v3a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1-.75-.75v-3c0-.966.784-1.75 1.75-1.75ZM6 5.25a.75.75 0 0 1 .75-.75h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 6 5.25Zm.75 2.25h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 6.75A.75.75 0 0 1 8.75 6h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 6.75ZM8.75 3h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 9.75A.75.75 0 0 1 8.75 9h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 9.75Zm-1 2.5v2.25h1v-2.25a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25Z"
+  );
+
+  svg.appendChild(path);
+  return svg;
 }
 
 function sizeFormat(sizeInKB) {
@@ -26,7 +41,7 @@ function listRepoSize(value, unit) {
   unitElem.className = "color-fg-muted";
   unitElem.textContent = ` ${unit}`;
 
-  anchor.innerHTML = fileZipSVG({ forSidebar: false });
+  anchor.appendChild(fizeZipSVG({ forSidebar: false }));
   anchor.appendChild(valueElem);
   anchor.appendChild(unitElem);
 
@@ -34,14 +49,13 @@ function listRepoSize(value, unit) {
 }
 
 function addToDetailsList(value, unit) {
-  const lastElem = document.querySelector(
-    '[aria-label="Repository details"]'
-  ).lastElementChild;
+  const activity = document
+    .querySelector(".Link--secondary .octicon-pulse")
+    .closest("a");
 
-  if (lastElem) {
+  if (activity) {
     const sizeElem = listRepoSize(value, unit);
-
-    lastElem.parentNode.insertBefore(sizeElem, lastElem);
+    activity.parentNode.insertBefore(sizeElem, activity);
   }
 }
 
@@ -55,7 +69,7 @@ function sidebarRepoSize(value, unit) {
   const valueElem = document.createElement("strong");
   valueElem.textContent = ` ${value}`;
 
-  anchor.innerHTML = fileZipSVG({ forSidebar: true });
+  anchor.appendChild(fizeZipSVG({ forSidebar: true }));
   anchor.appendChild(valueElem);
   anchor.appendChild(document.createTextNode(` ${unit}`));
 
@@ -71,7 +85,6 @@ function addToSidebar(value, unit) {
 
   if (forks) {
     const sizeElem = sidebarRepoSize(value, unit);
-
     forks.parentNode.insertBefore(sizeElem, forks.nextSibling);
   }
 }
